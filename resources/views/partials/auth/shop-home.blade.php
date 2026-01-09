83aabf06b420e1a5901744f0ac01955f2b536056
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-01-09 12:28:49 +0800

    right main div display

--- resources/views/partials/auth/shop-home.blade.php
+++ resources/views/partials/auth/shop-home.blade.php
@@ -1,8 +1,12 @@
 <div class="w-full bg-gray-200">
     @if ($admin)
-        <div class="admin_shop_main flex flex-row ">
+        <div class="admin_shop_main flex flex-row">
             <div class="shop_left">left</div>
-            <div class="shop_right">right</div>
+            <div class="shop_right p-5">
+                <div class="shop_right_content w-full bg-white">
+
+                </div>
+            </div>
         </div>
     @else
         <div>Shop for user</div>
